080102b2b817b0135127270e6987913906dd31d6
snowbar <snowbarlpt+tools@gmail.com>
2019-01-30 16:32:13 +0900

    add:deal with kanji

--- mktex.sh
+++ mktex.sh
@@ -3,8 +3,8 @@ filename=$1
 first=${filename%.*}
 last=${filename#*.}
 if [ ${last} = tex ]; then
-    platex ${first}.tex
-    platex ${first}.tex
+    platex -kanji=utf-8 ${first}.tex
+    platex -kanji=utf-8 ${first}.tex
     dvipdfmx ${first}.dvi
     rm ${first}.dvi ${first}.log ${first}.aux ${first}.toc
 fi
